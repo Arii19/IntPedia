b88374d425e26936d9173c26e23174ce7dbed042
Arii19 <arianer19@hotmail.com>
2025-04-20 22:25:01 -0300

    paginas e menus

---
+++ src/pages/Procedures.tsx
@@ -0,0 +1,11 @@
+function Procedures() {
+    return (
+      <div>
+        <h2 className="text-xl font-semibold mb-4">Procedures</h2>
+        <p>Aqui você pode visualizar ou cadastrar procedures do banco.</p>
+      </div>
+    );
+  }
+  
+  export default Procedures;
+  
